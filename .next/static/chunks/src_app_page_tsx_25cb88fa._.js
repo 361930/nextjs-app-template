57a19f5a3182fa78736997c50a3ef7d7988a5bd5
Mr. Khatri <117172329+361930@users.noreply.github.com>
2025-07-08 23:09:56 +0530

    Add .next/static/chunks/src_app_page_tsx_25cb88fa._.js 1751996399397

---
+++ .next/static/chunks/src_app_page_tsx_25cb88fa._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1c06f64a._.js",
+  "static/chunks/node_modules_cf798497._.js"
+],
+    source: "dynamic"
+});
